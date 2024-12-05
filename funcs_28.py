35eba8317c8e31a176650e7d4bdba0fdf0472a7b
oleg425top <oleg59152@gmail.com>
2024-12-05 23:20:44 +0500

    добавил хомяка

--- funcs_28.py
+++ funcs_28.py
@@ -3,7 +3,7 @@ print(my_list)
 # my_list_2 =list(map(lambda x: 2 ** x, my_list))
 my_list_2 =list(map(lambda x: 2 ** x, my_list))
 print(my_list_2)
-# print(type(my_list_2))
+# print(type_dog(my_list_2))
 
 my_gen = (x for x in range(5))
 my_gen_2 = list(map(lambda x: 2 ** x, my_gen))
