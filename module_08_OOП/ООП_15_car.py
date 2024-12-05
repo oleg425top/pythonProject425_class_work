04e384063a125a83989ddb07e1e50757ffa29060
oleg425top <oleg59152@gmail.com>
2024-12-05 23:01:14 +0500

    доработал программу

--- module_08_OOП/ООП_15_car.py
+++ module_08_OOП/ООП_15_car.py
@@ -3,11 +3,12 @@ class Wheels:
         self.__brand_w = brand_w
         self.__diameter = diameter
 
-    def diameter(self):
+    def get_diameter(self):
         return self.__diameter
 
-    def set_brand_w(self, new_diameter):
+    def set_diameter(self, new_diameter):
         self.__diameter = new_diameter
+        return f'вы поставили колеса другого диаметра {self.__diameter}'
 
     def get_brand_w(self):
         return self.__brand_w
@@ -23,6 +24,7 @@ class Engine:
 
     def set_capacity(self, new_capacity):
         self.__capacity = new_capacity
+        return f'(вы заменили двигатель на двигатель объемом: {self.__capacity} литра) '
 
     def get_brand_e(self):
         return self.__brand_e
@@ -38,12 +40,14 @@ class Doors:
 
     def set_door_trim(self, new_door_trim):
         self.__door_trim = new_door_trim
+        return f'вы заменили обивку дверей на: {self.__door_trim}'
 
     def get_count(self):
         return self.__count
 
+
 class Car(Wheels, Engine, Doors):
-    def __init__(self, brand_w, diameter, brand_e, capacity:int, count: int, door_trim, brand_car):
+    def __init__(self, brand_w, diameter, brand_e, capacity: int, count: int, door_trim, brand_car):
         super().__init__(brand_w, diameter)
         Engine.__init__(self, brand_e, capacity)
         Doors.__init__(self, count, door_trim)
@@ -53,13 +57,23 @@ class Car(Wheels, Engine, Doors):
         return self.__brand_car
 
     def __str__(self):
-        return (f'это машина марки {self.get_brand_car()} c диаметром колес {self.get_count()}\n'
-                f'марки {self.get_brand_w()} c двигателем марки {self.get_brand_e()}\n'
-                f'объемом {self.get_capacity()} литра, c {self.get_count()} дверьми\n'
-                f'материал обивки дверей {self.get_door_trim()}')
+        return (f'это машина марки: {self.get_brand_car()}, c диаметром колес: {self.get_diameter()}'
+                f', марка колес: {self.get_brand_w()}\nc двигателем марки: {self.get_brand_e()}\n'
+                f'объемом: {self.get_capacity()} литра, c {self.get_count()} дверьми\n'
+                f'материал обивки дверей: {self.get_door_trim()}')
+
 
 car = Car('Yokohama', '17 дюймов', 'Toyota', 3, 4, 'кожа', "Nissan-GTR")
-
 print(car)
-
-
+print()
+print(car.set_capacity(4))
+print()
+print(car)
+print()
+print(car.set_diameter(19))
+print()
+print(car)
+print()
+print(car.set_door_trim('эко кожа'))
+print()
+print(car)
